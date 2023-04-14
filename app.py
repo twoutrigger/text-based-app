e517a3bad9c98fc5ead9981634ff3cfcfc70f732
Liam M <twoutrigger@outlook.com>
2023-04-14 12:50:48 -0400

    basic start

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
+from models import person
 
-while True:
-    # sentence = "do you use credit cards?"
-    sentence = input("You: ")
-    if sentence == "quit":
-        break
 
-    
+def get_response(msg):
+    return "Hi"
+
+if __name__ == "__main__":
+    print("Let's get started! (type 'quit' to exit)")
+    print("\n")
+    print("What is your name?")
+    while True:
+        sentence = input("You: ")
+
+        if sentence == "quit":
+            break
+
+        resp = get_response(sentence)
+        print("Chatbot:" + resp)
