be32a710ecad2fa629db8d8bc9230f584224fbd8
Liam M <twoutrigger@outlook.com>
2023-04-15 16:16:46 -0400

    get_item using openai

--- steps/get_item.py
+++ steps/get_item.py
@@ -1,6 +1,31 @@
+import openai
 import sys
 sys.path.append('../')
-
 import config
+import json
 
-## accessing hidden creds
+def get_item(msg):
+        
+    ret_item = False
+        
+    openai.api_key = config.openai_api_key
+
+    prompt = f"Message: {msg}.\n For the message above, extract the physical items and put them into a json object, with the item name as the key, and item quantity as the value."
+
+    ret_item = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        max_tokens=40,
+        temperature=0.1
+    )
+
+    if ret_item:
+        ret_item =  ret_item['choices'][0]['text']
+        ret_item = json.loads(ret_item)
+
+    return ret_item
+
+
+# print(type(get_item('can I have 1 banana and two potatoes')))
+# print(get_item('can I have 1 banana and two potatoes'))
+# print(get_item('can I have a cheese cake please?'))
